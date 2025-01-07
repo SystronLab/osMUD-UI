ef189a3deed394279c6b6c991ca03066dd2e0d6d
Peter Gillingham <petergillingham80@gmail.com>
2025-01-07 12:41:05 +0000

    WIP

--- Makefile
+++ Makefile
@@ -7,9 +7,7 @@ dhcp_mock.build:
 	go build -o ${BUILD_DIR}/dhcp_mock ./tools/dhcp_mock
 
 api.build:
-	env CC=arm-linux-gnueabi-gcc CGO_ENABLED=1 GOOS=linux GOARCH=arm GOARM=7  \
-                go build -o ${BUILD_DIR}/api -ldflags "-linkmode 'external' -extldflags '-static'"\
-                -tags sqlite_omit_load_extension ./api
+	go build -o ${BUILD_DIR}/api ./api
 
 clean:
 	mkdir -p ${BUILD_DIR}
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-chmod +x ./db/reset_db.sh 
-./db/reset_db.sh 
+chmod +x ./scripts/db/reset_db.sh 
+./scripts/db/reset_db.sh 
 if [ $? -ne 0 ]; then
     echo -e "ERROR: Unable to reset mud database - are you using sudo?"
     exit 1
 fi
 
 
-$1 ../osmud/src/osmud -l /tmp/osmud/osmud.log -b /tmp/osmud/muds -e /var/log/osmud-dhcp-events.log -d -i
+$1 ./osmud/src/osmud -l /tmp/osmud/osmud.log -b /tmp/osmud/muds -e /var/log/osmud-dhcp-events.log -d -i
 if [ $? -ne 0 ]; then
     echo -e "ERROR: Ensure osmud is in PATH var - are you using sudo?"
     exit 1
