8a51e4d244bffd62a4815f7258d83e6e84059111
Peter Gillingham <petergillingham80@gmail.com>
2025-03-18 15:34:17 +0000

    add latest osmud build

--- graphs/mud_graph.py
+++ graphs/mud_graph.py
@@ -1,15 +1,14 @@
 ## Listing Devices
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 
 fontsize = 18
 linewidth = 2
 
 def extract_columns_from_csv(file_path, columns):
     df = pd.read_csv(file_path)
-    
     extracted_data = df[columns]
-    
     return extracted_data
 
 # Listing devices on the network 
@@ -20,12 +19,12 @@ def listing_devices():
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 8))
 
     # Plotting TAv with standard deviation
-    ax1.errorbar(data['Devices'], data['TAv'], yerr=data['SD'], fmt='-o', label='TAv', color='b', linewidth=linewidth)
+    ax1.errorbar(data['Devices'], data['TAv'], yerr=data['SD'], fmt='-o', label='Average Response Time', color='b', linewidth=linewidth)
     ax1.set_ylabel('Average Time (ms)', fontsize=fontsize)
     ax1.tick_params(axis='y', labelsize=fontsize)
     ax1.set_xticks([])
     ax1.set_title('Number of Devices vs Average Time', fontsize=fontsize)
-    ax1.legend(fontsize=fontsize)
+    ax1.legend(loc="upper left", fontsize=fontsize)
 
     # Plotting DB Size
     ax2.plot(data['Devices'], data['DB Size'], 'o--', label='DB Size', color='crimson', linewidth=linewidth)
@@ -33,26 +32,29 @@ def listing_devices():
     ax2.set_ylabel('Database Size (Bytes)', fontsize=fontsize)
     ax2.tick_params(axis='y', labelsize=fontsize)
     ax2.set_xticks(range(0, max(data['Devices']) + 1, 10))
+    ax2.tick_params(axis='x', labelsize=fontsize)
     # ax2.set_title('Number of Devices vs Database Size', fontsize=fontsize)
-    ax2.legend(fontsize=fontsize)
+    ax2.legend(loc="upper left", fontsize=fontsize)
 
     fig.tight_layout()
-    plt.savefig('graphs/listing-devices.png')
+    with PdfPages('graphs/listing-devices.pdf') as pdf:
+        pdf.savefig(fig)
+    plt.close(fig)
 
 # Applying a policy to a device
 def applying_policy():
     file_path = 'graphs/applying-policy.csv'
     data = extract_columns_from_csv(file_path, ['Policies', 'TAv', 'SD', 'MUD File Size'])
     
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 12))
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 8))
 
     # Plotting TAv with standard deviation
-    ax1.errorbar(data['Policies'], data['TAv'], yerr=data['SD'], fmt='-o', label='TAv', color='b', linewidth=linewidth)
+    ax1.errorbar(data['Policies'], data['TAv'], yerr=data['SD'], fmt='-o', label='Average Response Time', color='b', linewidth=linewidth)
     ax1.set_ylabel('Average Time (ms)', fontsize=fontsize)
     ax1.tick_params(axis='y', labelsize=fontsize)
     ax1.set_xticks([])
     ax1.set_title('Number of Policies vs Average Time', fontsize=fontsize)
-    ax1.legend("Average time to load device policies", fontsize=fontsize)
+    ax1.legend(loc="upper left", fontsize=fontsize)
 
     # Plotting MUD File Size
     ax2.plot(data['Policies'], data['MUD File Size'], 'o--', label='MUD File Size', color='crimson', linewidth=linewidth)
@@ -60,26 +62,29 @@ def applying_policy():
     ax2.set_ylabel('MUD File Size (Bytes)', fontsize=fontsize)
     ax2.tick_params(axis='y', labelsize=fontsize)
     ax2.set_xticks([2, 20, 40])
+    ax2.tick_params(axis='x', labelsize=fontsize)
     # ax2.set_title('Number of Policies vs MUD File Size', fontsize=fontsize)
-    ax2.legend(fontsize=fontsize)
+    ax2.legend(loc="upper left", fontsize=fontsize)
 
     fig.tight_layout()
-    plt.savefig('graphs/applying-policy.png')
+    with PdfPages('graphs/applying-policy.pdf') as pdf:
+        pdf.savefig(fig)
+    plt.close(fig)
     
 # Loading MUD file for a device
 def active_mudfiles():
     file_path = 'graphs/active-mudfiles.csv'
     data = extract_columns_from_csv(file_path, ['Policies', 'TAv', 'SD', 'MUD File Size'])
     
-    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 12))
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(12, 8))
 
     # Plotting TAv with standard deviation
-    ax1.errorbar(data['Policies'], data['TAv'], yerr=data['SD'], fmt='-o', label='TAv', color='b', linewidth=linewidth)
+    ax1.errorbar(data['Policies'], data['TAv'], yerr=data['SD'], fmt='-o', label='Average Response Time', color='b', linewidth=linewidth)
     ax1.set_ylabel('Average Time (ms)', fontsize=fontsize)
     ax1.tick_params(axis='y', labelsize=fontsize)
     ax1.set_xticks([])
     ax1.set_title('Number of Policies vs Average Time', fontsize=fontsize)
-    ax1.legend("Average time to apply policy to device", fontsize=fontsize)
+    ax1.legend(loc="upper left", fontsize=fontsize)
     
     # Plotting MUD File Size
     ax2.plot(data['Policies'], data['MUD File Size'], 'o--', label='MUD File Size', color='crimson', linewidth=linewidth)
@@ -87,11 +92,14 @@ def active_mudfiles():
     ax2.set_ylabel('MUD File Size (Bytes)', fontsize=fontsize)
     ax2.tick_params(axis='y', labelsize=fontsize)
     ax2.set_xticks([2, 20, 40])
+    ax2.tick_params(axis='x', labelsize=fontsize)
     # ax2.set_title('Number of Policies vs MUD File Size', fontsize=fontsize)
-    ax2.legend(fontsize=fontsize)
+    ax2.legend(loc="upper left", fontsize=fontsize)
 
     fig.tight_layout()
-    plt.savefig('graphs/active-mudfiles.png')
+    with PdfPages('graphs/active-mudfiles.pdf') as pdf:
+        pdf.savefig(fig)
+    plt.close(fig)
 
 listing_devices()
 applying_policy()
